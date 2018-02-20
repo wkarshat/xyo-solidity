e395e95d80fe702572eff36528397b7a593fa2be
Zypherx <raul@zk.capital>
2018-02-20 15:58:20 -0600

    order package example

--- contracts/XYExample.sol
+++ contracts/XYExample.sol
@@ -15,12 +15,18 @@ contract XYExample is XY, Ownable {
         ShippingStatus status;
     }
 
+    uint public numPackages;
     mapping (address => Package) public packages;
 
     // Orders and stores a package with an xyoAddress in this contract and
     // transfers the balance to this XYExample contract
     function orderPackage(address _xyoAddress) public payable returns(uint) {
-        // TODO: implement
+        require(_xyoAddress != address(0));
+        numPackages = numPackages + 1;
+        // transfers the payment to the owner contract
+        this.balance = msg.value;
+        packages[msg.sender] = Package(numPackages, _xyoAddress, ShippingStatus.Paid);
+        return numPackages;
     }
 
     // Tracks a packge by publishing a pending query to the XYO Contract that will
